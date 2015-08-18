3f978e6dfb55790d3c347459b709816a3592bea2
brookshi <iwxiaot@gmail.com>
2015-08-18 17:50:38 +0800

    add retry filter

---
+++ XPHttp/HttpRetryFilter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Web.Http;
+using Windows.Web.Http.Filters;
+
+namespace XPHttp
+{
+    public class HttpRetryFilter : IHttpFilter
+    {
+        private IHttpFilter _innerFilter;
+        public HttpRetryFilter(IHttpFilter innerFilter)
+        {
+            if(innerFilter == null)
+            {
+                throw new ArgumentNullException("innerFilter cannot be null.");
+            }
+            _innerFilter = innerFilter;
+        }
+
+        public IAsyncOperationWithProgress<HttpResponseMessage, HttpProgress> SendRequestAsync(HttpRequestMessage request)
+        {
+            return AsyncInfo.Run<HttpRequestMessage, HttpProgress>(async (cancellationToken, progress) =>
+            {
+                HttpResponseMessage response = await _innerFilter.SendRequestAsync(request).AsTask(cancellationToken, progress);
+
+                return response;
+            });
+        }
+
+        public void Dispose()
+        {
+            _innerFilter.Dispose();
+        }
+    }
+}
--- XPHttp/XPHttpConfig.cs
+++ XPHttp/XPHttpConfig.cs
@@ -3,17 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Web.Http;
 using Windows.Web.Http.Filters;
 
 namespace XPHttp
 {
     public class XPHttpConfig
     {
+        private static HttpStatusCode[] _defaultHttpCodeForRetry = { HttpStatusCode.ServiceUnavailable };
+
         public string BaseUrl { get; set; }
+
         public Dictionary<string, string> DefaultHeaders { get; set; } = new Dictionary<string, string>();
+
         public int TimeOut { get; set; } = 30;
-        public int RetryTimes { get; set; } = 3;
+
+        public int Retries { get; set; } = 3;
+
+        public List<HttpStatusCode> RetryForHttpCodes { get; set; } = new List<HttpStatusCode>(_defaultHttpCodeForRetry);
+
         public Func<DateTime, string> DateFormatter { get; set; } = dateTime => { return dateTime.ToString("yyyy-MM-dd"); };
+
         public IHttpFilter CustomHttpFilter { get; set; }
 
         public static XPHttpConfig Builder { get { return new XPHttpConfig(); } }
@@ -38,7 +48,7 @@ namespace XPHttp
 
         public XPHttpConfig SetRetryTimes(int retryTimes)
         {
-            RetryTimes = retryTimes;
+            Retries = retryTimes;
             return this;
         }
 
