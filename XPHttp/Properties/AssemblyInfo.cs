c5ebfd0a1bb9c334ba2e62b2ef7f506a4618a467
brookshi <iwxiaot@gmail.com>
2015-09-03 18:43:55 +0800

    add to nuget

--- XPHttp/Properties/AssemblyInfo.cs
+++ XPHttp/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("UWP_DataRequester")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("XPHttp")]
+[assembly: AssemblyDescription("An asynchronous, callback-based Http client for UWP (Universal Windows Platform)")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("UWP_DataRequester")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCompany("BrookShi")]
+[assembly: AssemblyProduct("XPHttp")]
+[assembly: AssemblyCopyright("Copyright ©  2015 Brook Shi")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -24,6 +24,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.2.1.0")]
+[assembly: AssemblyFileVersion("0.2.1.0")]
 [assembly: ComVisible(false)]
